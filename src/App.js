41a9e004ae3556d8c66554f971539fe3768fb224
benjamin creniaut <creniaut.benjamin@gmail.com>
2022-06-29 15:18:50 +0200

    v2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/header/header";
 import Background from "./components/background/background";
 import "./App.css";
 import AudioPlayer, { RHAP_UI } from "react-h5-audio-player";
+import { Helmet } from "react-helmet";
 
 function App() {
   const [data, setData] = useState();
@@ -10,18 +11,18 @@ function App() {
   const [src, setSrc] = useState();
   const [musicName, setMusicName] = useState();
   const [indexMusic, setIndexMusic] = useState(0);
-  const [randomizeData, setRandomizeData] = useState();
   const [videoSrc, setVideoSrc] = useState();
   const [reload, setReload] = useState(false);
 
+  const [onAir, setOnAir] = useState(true);
+
   const player = useRef();
-  const url = "http://13.38.89.2:1337";
+  const url = "https://back.testbenjaminc.fr/";
 
   useEffect(() => {
     async function fetchMyAPI() {
       let response = await fetch(
-        `http://13.38.89.2:1337/api/playlists?populate=*`,
-        {}
+        `https://back.testbenjaminc.fr/api/playlists?populate=*`
       );
       response = await response.json();
       setData(response.data);
@@ -34,86 +35,107 @@ function App() {
   }, []);
 
   useEffect(() => {
-    console.log(reload);
     if (player.current !== undefined && currentStatus !== undefined) {
       startPlayer();
     }
   }, [reload]);
 
   function getTheStatus(data) {
+    setIndexMusic(0);
     const hour = new Date().getHours();
-    data.forEach((element) => {
+    let night;
+    data.forEach((element, index) => {
+      console.log(index);
+      if (element.attributes.name === "nuit") {
+        night = element;
+      }
       if (
-        hour >= element.attributes.mintime &&
-        hour < element.attributes.maxtime
+        hour >= element.attributes.minTime &&
+        hour < element.attributes.maxTime
       ) {
-        setCurrentStatus(element);
+        let newDatas = randomize(element);
+        setCurrentStatus(newDatas);
         setReload(!reload);
         setVideoSrc(url + element.attributes.background.data.attributes.url);
-
+        setOnAir(true);
         return;
+      } else {
+        if (index === 2) {
+          let newDatas = randomize(night);
+          setCurrentStatus(newDatas);
+          setReload(!reload);
+          setVideoSrc(url + night.attributes.background.data.attributes.url);
+          setOnAir(true);
+        }
       }
     });
   }
 
   function switchStatus(e, status) {
+    setOnAir(false);
+    setIndexMusic(0);
     document.querySelectorAll(".headerTitle").forEach((elem) => {
       elem.classList.remove("activeTitle");
     });
     e.target.classList.add("activeTitle");
     data.forEach((element) => {
+      if (status === "all") {
+        getTheStatus(data);
+        return;
+      }
       if (element.attributes.name === status) {
-        setCurrentStatus(element);
+        let newDatas = randomize(element);
+        setCurrentStatus(newDatas);
         setReload(!reload);
         setVideoSrc(url + element.attributes.background.data.attributes.url);
         return;
       }
-      if (status === "all") {
-        getTheStatus(data);
-      }
     });
   }
 
-  function randomize() {
-    let list = currentStatus.attributes.music.data.map((value, index) => index);
+  function randomize(dataSet) {
+    let list = dataSet.attributes.musique.data.map((value, index) => index);
     list = list.sort(() => Math.random() - 0.5);
     const results = list.map(
-      (number) => currentStatus.attributes.music.data[number]
+      (number) => dataSet.attributes.musique.data[number]
     );
     return results;
   }
 
   function startPlayer() {
-    let results = randomize();
-    setRandomizeData(results);
-    let secondPartSrc = results[indexMusic].attributes.src;
-    setIndexMusic(0);
+    let secondPartSrc = currentStatus[indexMusic].attributes.url;
     setSrc(url + secondPartSrc);
-    setMusicName(results[indexMusic].attributes.name);
+    setMusicName(currentStatus[indexMusic].attributes.name);
   }
 
   function nextMusic() {
-    if (indexMusic >= randomizeData.length - 1) {
+    if (indexMusic >= currentStatus.length - 1) {
       setIndexMusic(0);
     } else {
       setIndexMusic(indexMusic + 1);
     }
 
-    let secondPartSrc = randomizeData[indexMusic].attributes.src;
+    let secondPartSrc = currentStatus[indexMusic].attributes.src;
     setSrc(url + secondPartSrc);
-    setMusicName(randomizeData[indexMusic].attributes.name);
+    setMusicName(currentStatus[indexMusic].attributes.name);
     setReload(!reload);
   }
 
   function previousMusic(e) {
     setIndexMusic(indexMusic - 1);
-    let secondPartSrc = randomizeData[indexMusic].attributes.src;
+    console.log(indexMusic);
+    let secondPartSrc = currentStatus[indexMusic].attributes.src;
     setSrc(url + secondPartSrc);
-    setMusicName(randomizeData[indexMusic].attributes.name);
+    setMusicName(currentStatus[indexMusic].attributes.name);
+    setReload(!reload);
   }
 
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Maison Palmier</title>
+      </Helmet>
       <Header switchStatus={switchStatus} />
       {currentStatus == undefined ? (
         <></>
@@ -121,27 +143,58 @@ function App() {
         <>
           <Background videoSrc={videoSrc} />
           <div className="audioContainer">
-            {/* <h1>{player.current.audio.current.currentTime}</h1>
-            <h1>{player.current.audio.current.duration}</h1> */}
-
-            <AudioPlayer
-              ref={player}
-              showSkipControls={true}
-              showJumpControls={false}
-              autoPlay
-              src={src}
-              onClickNext={(e) => nextMusic(e)}
-              onClickPrevious={(e) => previousMusic(e)}
-              onEnded={(e) => nextMusic(e)}
-              customProgressBarSection={[
-                RHAP_UI.PROGRESS_BAR,
-                RHAP_UI.CURRENT_TIME,
-                <div className="separatorTime">:</div>,
-                RHAP_UI.DURATION,
-              ]}
-            />
+            {onAir ? (
+              <AudioPlayer
+                ref={player}
+                showSkipControls={false}
+                showJumpControls={false}
+                autoPlay
+                src={src}
+                onClickNext={(e) => nextMusic(e)}
+                onClickPrevious={(e) => previousMusic(e)}
+                onEnded={(e) => nextMusic(e)}
+                customProgressBarSection={[
+                  RHAP_UI.PROGRESS_BAR,
+                  RHAP_UI.CURRENT_TIME,
+                  <div className="separatorTime">:</div>,
+                  RHAP_UI.DURATION,
+                ]}
+              />
+            ) : (
+              <AudioPlayer
+                ref={player}
+                showSkipControls={true}
+                showJumpControls={false}
+                autoPlay
+                src={src}
+                onClickNext={(e) => nextMusic(e)}
+                onClickPrevious={(e) => previousMusic(e)}
+                onEnded={(e) => nextMusic(e)}
+                customProgressBarSection={[
+                  RHAP_UI.PROGRESS_BAR,
+                  RHAP_UI.CURRENT_TIME,
+                  <div className="separatorTime">:</div>,
+                  RHAP_UI.DURATION,
+                ]}
+              />
+            )}
             <div className="musicName">
-              <p>{musicName}</p>
+              {musicName ? (
+                <p>
+                  {musicName
+                    .replace(/ *\([^)]*\) */g, "")
+                    .replace("MPJournée", "")
+                    .replace("MPMatinée", "")
+                    .replace("MPSoirée", "")
+                    .replace(".mp3", "")
+                    .replace(/[\[\]']+/g, "")
+                    .replace("-", " ")
+                    .replace("_", " ")
+                    .replace("MPSoirée -", " ")}
+                </p>
+              ) : (
+                <></>
+              )}
             </div>
           </div>
         </>
